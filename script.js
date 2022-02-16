ad40cb6b5bb2880d93f38ac015954ec584d40a28
IGS <99769515+IonicGameStudios@users.noreply.github.com>
2022-02-15 21:18:16 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function daplink()
+{
+   window.open( "https://github.com/22boohoo", "_blank");
+}
+function nolaneolink()
+{
+  window.open("https://github.com/GDSlatherycGithub", "_blank");
+}
+function zixenarlink()
+{
+  window.open("https://github.com/Zixenar", "_blank");
+}
+function gamemakerlink()
+{
+  window.open("https://github.com/Xavier333322211", "_blank");
+}
+function game1()
+{
+  window.open()
+}
+function igsgit()
+{
+  window.open("https://github.com/IonicGameStudios", "_blank");
+}
+function igsrepl()
+{ window.open("https://replit.com/@GDSlatheryc/igsgithubio")
+}
+function playcanvas()
+{
+alert("This is a website not associated with us. Proceed under the assumption that we act differently than them.")
+window.open("https://playcanvas.com")
+}
